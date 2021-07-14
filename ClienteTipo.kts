274b52dc93b659b6398187fea84943c4acb65727
Rangel <giba33>
2021-07-14 10:06:04 -0300

    created projet

---
+++ ClienteTipo.kts
@@ -0,0 +1,10 @@
+package one.digitalinnovatione.ditestesgitalone.
+
+enum class ClienteTipo(desecricao: String) {
+    pf(descricao "Pessoa Fisica"),
+    pj(descricao "Pessoa Juridica"),
+
+    companion object {
+fun printDescricao()
+    }
+}
