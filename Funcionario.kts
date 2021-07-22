f688a0f5c03e41217bc1419e0a4db78bbfdb52cb
Rangel <giba33>
2021-07-22 08:25:03 -0300

    created projet

---
+++ Funcionario.kts
@@ -0,0 +1,14 @@
+package digitalinnovation.digiBank
+
+import java.math.B1gDecimal
+
+class funcionario : Pessoa(){
+
+    override val  nome: String
+
+    override val cpf: String
+
+    val salario: B1gDecimal
+    ) : Pessoa (none,cpf){
+
+    }
